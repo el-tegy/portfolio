b34806c12abb4f19264243d06c9d32e0d39de5fc
el-tegy <elisee.tegue@groupe-esigelec.org>
2023-11-06 18:45:29 +0100

    feat:

--- src/components/Home/Home2.js
+++ src/components/Home/Home2.js
@@ -4,8 +4,7 @@ import myImg from "../../Assets/avatar.svg";
 import Tilt from "react-parallax-tilt";
 import {
   AiFillGithub,
-  AiOutlineTwitter,
-  AiFillInstagram,
+  AiOutlineMail
 } from "react-icons/ai";
 import { FaLinkedinIn } from "react-icons/fa";
 
@@ -19,37 +18,43 @@ function Home2() {
               LET ME <span className="purple"> INTRODUCE </span> MYSELF
             </h1>
             <p className="home-about-body">
-              I fell in love with programming and I have at least learnt
-              something, I think… 🤷‍♂️
+              I fell in love with Data Engineering and I have at least learnt
+              something (I think…🤷‍♂️)
               <br />
               <br />I am fluent in classics like
               <i>
-                <b className="purple"> C++, Javascript and Go. </b>
+                <b className="purple"> Python, Spark and SQL. </b>
               </i>
               <br />
               <br />
-              My field of Interest's are building new &nbsp;
+              My fields of interest are&nbsp;
               <i>
-                <b className="purple">Web Technologies and Products </b> and
-                also in areas related to{" "}
+                <b className="purple">building new ETL pipelines </b> 
+                and{" "}
                 <b className="purple">
-                  Blockchain.
+                implementing robust data architectures{" "}
                 </b>
+                that can handle 
+                the influx of big data.{" "}
               </i>
               <br />
               <br />
-              Whenever possible, I also apply my passion for developing products
-              with <b className="purple">Node.js</b> and
+              Moreover, I have a keen interest in
+              <i>
+                <b className="purple"> developing real-time data processing 
+                frameworks </b>
+              </i> 
+              that enable businesses to make timely, 
+              data-driven decisions. As a data engineer, I also{" "} 
               <i>
                 <b className="purple">
-                  {" "}
-                  Modern Javascript Library and Frameworks
-                </b>
-              </i>
-              &nbsp; like
+                stay abreast of developments in cloud technologies </b>
+              </i> 
+              and continuously explore ways to{" "} 
               <i>
-                <b className="purple"> React.js and Next.js</b>
+                <b className="purple">improve data security and compliance{" "}</b>
               </i>
+              in an ever-evolving digital landscape.
             </p>
           </Col>
           <Col md={4} className="myAvtar">
@@ -67,7 +72,7 @@ function Home2() {
             <ul className="home-about-social-links">
               <li className="social-icons">
                 <a
-                  href="https://github.com/soumyajit4419"
+                  href="https://github.com/el-tegy"
                   target="_blank"
                   rel="noreferrer"
                   className="icon-colour  home-social-icons"
@@ -77,17 +82,7 @@ function Home2() {
               </li>
               <li className="social-icons">
                 <a
-                  href="https://twitter.com/Soumyajit4419"
-                  target="_blank"
-                  rel="noreferrer"
-                  className="icon-colour  home-social-icons"
-                >
-                  <AiOutlineTwitter />
-                </a>
-              </li>
-              <li className="social-icons">
-                <a
-                  href="https://www.linkedin.com/in/soumyajit4419/"
+                  href="https://fr.linkedin.com/in/elis%C3%A9e-tegue-yombi-a1455720a"
                   target="_blank"
                   rel="noreferrer"
                   className="icon-colour  home-social-icons"
@@ -97,12 +92,12 @@ function Home2() {
               </li>
               <li className="social-icons">
                 <a
-                  href="https://www.instagram.com/soumyajit4419"
+                  href="eliseetegue@gmail.com"
                   target="_blank"
                   rel="noreferrer"
                   className="icon-colour home-social-icons"
                 >
-                  <AiFillInstagram />
+                  <AiOutlineMail />
                 </a>
               </li>
             </ul>
