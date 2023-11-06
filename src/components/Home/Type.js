d11e6e34fc116a548ac3bd91b65326c457d64dd1
el-tegy <elisee.tegue@groupe-esigelec.org>
2023-11-06 18:45:37 +0100

    feat:

--- src/components/Home/Type.js
+++ src/components/Home/Type.js
@@ -6,10 +6,10 @@ function Type() {
     <Typewriter
       options={{
         strings: [
-          "Software Developer",
-          "Freelancer",
-          "MERN Stack Developer",
-          "Open Source Contributor",
+          "Data Engineer",
+          "DevOps Engineer",
+          "Databricks Certified Data Engineer",
+          "AWS Certified Developer - Associate",
         ],
         autoStart: true,
         loop: true,
