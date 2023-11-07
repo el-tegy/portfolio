d6486d9160768ff9f05e05b2318f1316a13e5f7e
el-tegy <elisee.tegue@groupe-esigelec.org>
2023-11-07 13:04:45 +0100

    feat: Add chatBI project

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -5,7 +5,7 @@ import Particle from "../Particle";
 import leaf from "../../Assets/Projects/leaf.png";
 import emotion from "../../Assets/Projects/emotion.png";
 import editor from "../../Assets/Projects/codeEditor.png";
-import chatify from "../../Assets/Projects/chatify.png";
+import chatBI from "../../Assets/Projects/chatBI.png";
 import suicide from "../../Assets/Projects/suicide.png";
 import bitsOfCode from "../../Assets/Projects/blog.png";
 
@@ -23,12 +23,18 @@ function Projects() {
         <Row style={{ justifyContent: "center", paddingBottom: "10px" }}>
           <Col md={4} className="project-card">
             <ProjectCard
-              imgPath={chatify}
+              imgPath={chatBI}
               isBlog={false}
-              title="Chatify"
-              description="Personal Chat Room or Workspace to share resources and hangout with friends build with react.js, Material-UI, and Firebase. Have features which allows user for realtime messaging, image sharing as well as supports reactions on messages."
-              ghLink="https://github.com/soumyajit4419/Chatify"
-              demoLink="https://chatify-49.web.app/"
+              title="ChatBI"
+              description="A final-year engineering project involving the creation 
+              of a Large Language Model (LLM) based on GPT-4 that will be capable of 
+              assisting the BI engineer throughout the entire delivery chain: from analysing 
+              the downstream business need and choosing the appropriate type of visualizations
+              to creating and interpreting those visualizations. With my 05 teammates, we plan 
+              to integrate our tool with Power BI initially, before extending it to Tableau and 
+              Qlik Sense later on."
+              ghLink="https://github.com/el-tegy/chatBI"
+              //demoLink="https://chatify-49.web.app/"
             />
           </Col>
 
