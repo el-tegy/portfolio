398294f65191737064b5b140c7d7c735167392a4
el-tegy <67525407+el-tegy@users.noreply.github.com>
2023-11-08 13:13:52 +0000

    feat: Update icons and add conditional rendering for credential link and not available message

--- src/components/Achievements/AchievementCards.js
+++ src/components/Achievements/AchievementCards.js
@@ -1,8 +1,8 @@
 import React from "react";
 import Card from "react-bootstrap/Card";
 import Button from "react-bootstrap/Button";
-import { CgWebsite } from "react-icons/cg";
-import { BsGithub } from "react-icons/bs";
+import { TbCertificate } from "react-icons/tb";
+import { BsHourglassSplit } from "react-icons/bs";
 
 function AchievementCards(props) {
     return (
@@ -13,28 +13,31 @@ function AchievementCards(props) {
                 <Card.Text style={{ textAlign: "justify" }}>
                     {props.description}
                 </Card.Text>
-                <Button variant="primary" href={props.ghLink} target="_blank">
-                    <BsGithub /> &nbsp;
-                    {props.isBlog ? "Blog" : "GitHub"}
-                </Button>
                 {"\n"}
                 {"\n"}
 
-                {/* If the component contains Demo link and if it's not a Blog then, it will render the below component  */}
+                {!props.isBlog && props.cdLink && (
+                    <Button variant="primary" href={props.cdLink} target="_blank">
+                        <TbCertificate /> &nbsp;
+                        {props.isBlog ? "Blog" : "View my credential"}
+                    </Button>
+                )}
 
-                {!props.isBlog && props.demoLink && (
+                {/* If the component contains Demo link and it's not a Blog, it will render the below component without target="_blank" to open in the same tab */}
+                
+                {!props.isBlog && props.notAv && (
                     <Button
                         variant="primary"
                         href={props.demoLink}
-                        target="_blank"
                         style={{ marginLeft: "10px" }}
                     >
-                        <CgWebsite /> &nbsp;
-                        {"Demo"}
+                        <BsHourglassSplit /> &nbsp;
+                        {"Not available yet - coming soon!"}
                     </Button>
                 )}
             </Card.Body>
         </Card>
     );
 }
+
 export default AchievementCards;
